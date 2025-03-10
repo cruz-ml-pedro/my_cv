22b43a25f5160e6676c0770852f40b0eab6b52c5
cruz-ml-pedro <cruz.ml.pedro@gmail.com>
2025-03-10 17:00:55 -0300

    CV update

--- R/data.R
+++ R/data.R
@@ -1,40 +1,36 @@
 
 work <- tribble(
   ~title, ~unit, ~startMonth, ~startYear, ~endMonth, ~endYear, ~where, ~detail,
-  "Autônomo", "Projéto de ciências de Dados", "Julho", 2024, "Presente",2024 , "São Paulo - SP", "Linguagens e tecnologias usadas --- R, Python",
-  "Autônomo", "Projéto de ciências de Dados", "Julho", 2024, "Presente", 2024, "São Paulo - SP", "Principais pontos --- Criação de métricas de desempenho de times e jogadores, modelos estatísticos e de IA.",
-  "Autônomo", "Projéto de ciências de Dados", "Julho", 2024, "Presente", 2024, "São Paulo - SP", "Focando no mercado de apostas esportivas e na otimização de estratégias de betting.",
+
+  "Freelancer", "Quantum Internacional/Grupo de vendedores", "Outubro", 2024, "Janeiro", 2025, "São Paulo - SP", "Desenvolvimento de scripts de web scraping para coleta de dados do Mercado Livre, integração com a API da plataforma para extração de informações detalhadas, e análise de tendências de buscas via Google Trends, visando avaliar a viabilidade e oportunidades estratégicas para a criação de um e-commerce.",
+  "Freelancer", "Quantum Internacional/Grupo de vendedores", "Outubro", 2024, "Janeiro", 2025, "São Paulo - SP", "Elaboração de relatório investigativo com análise de dados de preços de lojas concorrentes, identificação de produtos mais vendidos por categoria, análise de cesta de compras a partir de recomendações do site, monitoramento de volumes de busca e tendências ao longo do tempo, identificação de sazonalidades e estimativa de volume de vendas, visando embasar decisões estratégicas para o e-commerce.",
+  "Freelancer", "Quantum Internacional/Grupo de vendedores", "Outubro", 2024, "Janeiro", 2025, "São Paulo - SP","Linguagens e tecnologias usadas --- R/Rstudio, Quarto",
+ 
+  "Freelancer", "iCred-Crédito Inteligente", "Setembro", 2022, "Novembro", 2022, "São Paulo - SP", "Desenvolvimento de scripts de web scraping para coleta de dados estratégicos, como análise de SERP com base em palavras-chave relacionadas aos serviços da empresa, monitoramento de concorrentes em plataformas como Reclame Aqui e extração de insights de tendências via Google Trends, visando inteligência competitiva e suporte à tomada de decisão  baseada em dados.",
+  "Freelancer", "iCred-Crédito Inteligente", "Setembro", 2022, "Novembro", 2022, "São Paulo - SP", "Principais pontos --- Criação de relatórios analíticos  com aplicação de Text Mining para processamento de dados textuais, análise de séries temporais para monitoramento de tendências de palavras-chave e visualização geoespacial de dados de buscas.",
+  "Freelancer", "iCred-Crédito Inteligente", "Setembro", 2022, "Novembro", 2022, "São Paulo - SP", "Linguagens e tecnologias usadas --- R/Rstudio, Shiny, RMarkdown, Docker",
   
+  "Freelancer", "FESPSP-SEBRAE", "Março", 2022, "Junho", 2022, "São Paulo - SP", "Desenvolvimento de scripts para ETL de dados de arquivos .xlsx, automação de relatórios mensais, gestão de despesas, disparo de emails e atualização de bases de dados, visando otimização de processos e análise de dados.",
+  "Freelancer", "FESPSP-SEBRAE", "Março", 2022, "Junho", 2022, "São Paulo - SP", "Desenvolvimento de dashboard interativo para monitoramento de KPIs de projetos e gestão de custos.",
+  "Freelancer", "FESPSP-SEBRAE", "Março", 2022, "Junho", 2022, "São Paulo - SP", "Implementação de um blog corporativo para centralização de comunicados internos, FAQs e tutoriais, visando a melhoria da comunicação organizacional.",
+  "Freelancer", "FESPSP-SEBRAE", "Março", 2022, "Junho", 2022, "São Paulo - SP", "Linguagens e tecnologias usadas --- R/Rstudio, RMarkdown, blogdown,Docker, Google Sheets",
   
-  "Autônomo", "", "Dezembro", 2022, "Presente", 2024,"São Paulo - SP","Atividades não relacionadas com dados.",
-  
-  "Freelancer", "iCred-Crédito Inteligente", "Setembro", 2022, "Novembro", 2022, "São Paulo - SP", "Linguagens e tecnologias usadas --- R/Rstudio, Shiny, RMarkdown",
-  "Freelancer", "iCred-Crédito Inteligente", "Setembro", 2022, "Novembro", 2022, "São Paulo - SP", "Principais pontos --- Mapas interativos, Text Mining, Forecast de Séries Temporais",
-  "Freelancer", "iCred-Crédito Inteligente", "Setembro", 2022, "Novembro", 2022, "São Paulo - SP", "Desenvolvimento de aplicações para coleta, análise e criação de relatórios sobre dados provenientes de APIs e web scraping.",
-  "Freelancer", "iCred-Crédito Inteligente", "Setembro", 2022, "Novembro", 2022, "São Paulo - SP", "Aplicação voltada para o suporte das estratégias de marketing digital.",
-  
-  "Freelancer", "FESPSP-SEBRAE", "Março", 2022, "Junho", 2022, "São Paulo - SP", "Linguagens e tecnologias usadas --- R/Rstudio, RMarkdown, blogdown, Google Sheets",
-  "Freelancer", "FESPSP-SEBRAE", "Março", 2022, "Junho", 2022, "São Paulo - SP", "Automatização do fluxo de trabalho --- \"ETL\" - criação de relatórios, recibos e emails",
-  "Freelancer", "FESPSP-SEBRAE", "Março", 2022, "Junho", 2022, "São Paulo - SP", "Criação de dashboard interativo para acompanhamento de projetos.",
-  "Freelancer", "FESPSP-SEBRAE", "Março", 2022, "Junho", 2022, "São Paulo - SP", "Criação de um blog para plublicação de comunicados internos, dúvidas frequentes e tutoriais.",
-  
-  
-  "Autônomo", "Projétos de automação", "Agosto", 2020, "Dezembro", 2020, "São Paulo - SP", "Linguagens e tecnologias usadas --- Arduino (C++), Diferentes Sensores",
   "Autônomo", "Projétos de automação", "Agosto", 2020, "Dezembro", 2020, "São Paulo - SP", "Criação e implementação de sistemas estilo Garduino.",
+  "Autônomo", "Projétos de automação", "Agosto", 2020, "Dezembro", 2020, "São Paulo - SP", "Linguagens e tecnologias usadas --- Arduino (C++), Diferentes Sensores",
   
-  "Geofísico Júnior", "Geodecon Geologia e Geofísica Ltda", "Janeiro", 2016, "Outubro", 2016, "Curitiba - PR", "Métodos aplicados --- Eletrorresistividade, Magnetometria, Sísmica rasa",
-  "Geofísico Júnior", "Geodecon Geologia e Geofísica Ltda", "Janeiro", 2016, "Outubro", 2016, "Curitiba - PR", "Softwares usados --- Geosoft Oasis Montaj, Geometrics analysis softwares",
-  "Geofísico Júnior", "Geodecon Geologia e Geofísica Ltda", "Janeiro", 2016, "Outubro", 2016, "Curitiba - PR", "Áreas de aplicação --- Passivos ambientais, suporte a obras de engenharia, prospecção mineral e de águas subterrâneas",
+  "Geofísico Júnior", "Geodecon Geologia e Geofísica Ltda", "Janeiro", 2016, "Outubro", 2016, "Curitiba - PR", "Processamento de dados, criação de modelos geofísicos, elaboração de relatórios.",
   "Geofísico Júnior", "Geodecon Geologia e Geofísica Ltda", "Janeiro", 2016, "Outubro", 2016, "Curitiba - PR", "Aquisição e controle de qualidade dos dados geofísicos e topográficos.",
-  "Geofísico Júnior", "Geodecon Geologia e Geofísica Ltda", "Janeiro", 2016, "Outubro", 2016, "Curitiba - PR", "Processamento de dados, elaboração de relatórios, Criação de modelos de inversão.",
+  "Geofísico Júnior", "Geodecon Geologia e Geofísica Ltda", "Janeiro", 2016, "Outubro", 2016, "Curitiba - PR", "Áreas de aplicação --- Passivos ambientais, suporte a obras de engenharia, prospecção mineral e de águas subterrâneas",
+  "Geofísico Júnior", "Geodecon Geologia e Geofísica Ltda", "Janeiro", 2016, "Outubro", 2016, "Curitiba - PR", "Softwares usados --- Geosoft Oasis Montaj, Geometrics analysis softwares",
+  "Geofísico Júnior", "Geodecon Geologia e Geofísica Ltda", "Janeiro", 2016, "Outubro", 2016, "Curitiba - PR", "Métodos aplicados --- Eletrorresistividade, Magnetometria, Sísmica rasa",
   
   "Geofísico-Estágio", "Geodecon Geologia e Geofísica Ltda", "Junho", 2015, "Dezembro", 2015, "Curitiba - PR", "Aquisição e controle de qualidade dos dados geofísicos e topográficos.",
   
-  "Analista Ambiental Trainee", "Ecotest Tecnologia Ambiental Ltda", "", 2010, "", 2011, "São Paulo - SP", "Softwares usados --- Word, Excel, Surfer (Golden software), AutoCad",
-  "Analista Ambiental Trainee", "Ecotest Tecnologia Ambiental Ltda", "", 2010, "", 2011, "São Paulo - SP", "Áreas de atuação --- Investigação de passivos e remediação ambiental",
-  "Analista Ambiental Trainee", "Ecotest Tecnologia Ambiental Ltda", "", 2010, "", 2011, "São Paulo - SP", "Elaboração de relatórios técnicos para orgãos ambientais.",
-  "Analista Ambiental Trainee", "Ecotest Tecnologia Ambiental Ltda", "", 2010, "", 2011, "São Paulo - SP", "Elaboração de modelos de difusão, analíticos e numéricos.",
+  "Analista Ambiental Trainee", "Ecotest Tecnologia Ambiental Ltda", "", 2010, "", 2011, "São Paulo - SP", "Desenvolvimento de modelos analíticos e numéricos para interpolação espacial e difusão.",
   "Analista Ambiental Trainee", "Ecotest Tecnologia Ambiental Ltda", "", 2010, "", 2011, "São Paulo - SP", "Responsável pela gestão do fluxo de trabalho relacionado as amostras de água e solo dos projetos.",
+  "Analista Ambiental Trainee", "Ecotest Tecnologia Ambiental Ltda", "", 2010, "", 2011, "São Paulo - SP", "Elaboração de relatórios técnicos para orgãos ambientais.",
+  "Analista Ambiental Trainee", "Ecotest Tecnologia Ambiental Ltda", "", 2010, "", 2011, "São Paulo - SP", "Áreas de atuação --- Investigação de passivos e remediação ambiental",
+  "Analista Ambiental Trainee", "Ecotest Tecnologia Ambiental Ltda", "", 2010, "", 2011, "São Paulo - SP", "Softwares usados --- Word, Excel, Surfer (Golden software), AutoCad" ,
   
   "Técnico de campo II", "Ecotest Tecnologia Ambiental Ltda", "", 2009, "", 2010, "São Paulo - SP", "Montagem de kits de amostragem para água e solo. Controle de qualidade e registro das amostras coletadas",
   "Técnico de campo II", "Ecotest Tecnologia Ambiental Ltda", "", 2009, "", 2010, "São Paulo - SP", "Controle do envio das amostras para análise e recebimento dos relatórios químicos",
@@ -46,9 +42,9 @@ work <- tribble(
   
 )
 
-
 edu <- tribble(
-  ~degree, ~startYear, ~endYear, ~inst, ~where, ~detail,
+  ~inst, ~startYear, ~endYear, ~degree, ~where, ~detail,
+  "Universidade Estácio", 2025, 2027, "Técnologo em Ciência de Dados", "São Paulo - SP","1º Semestre",
   "Instituto Tecnológico de Aeronáutica - ITA", 2017, 2021, "Mestre em Ciências", "São José dos Campo - SP","Área de concentração - Física e Matemática Aplicadas",
   "Instituto Tecnológico de Aeronáutica - ITA", 2017, 2021, "Mestre em Ciências", "São José dos Campo - SP","Bolsista CAPES",
   "Instituto Tecnológico de Aeronáutica - ITA", 2017, 2021, "Mestre em Ciências", "São José dos Campo - SP","Curso atrasado em três semestres devido a trancamento e pandemia da covid-19",
@@ -79,26 +75,13 @@ research <- tribble(
 
 
 
-
 skills <- tribble(
   ~area, ~skills,
-  "Linguagens de Programação", "R",
-  "Linguagens Markup","Quarto",
-  "Web Framework", "Shiny",
-  "Dados", "Spreadsheet"
+  "Linguagens de Programação", "R, SQL",
+  "Outras tecnologias","Quarto, Shiny, Docker",
+  "Aprendendo", "Python, Power BI (DAX, M), Apache Spark (Sparklyr)"
 )
 
-basico <- tribble(
-  ~area, ~skills,
-  "Linguagens Markup","HTML, CSS",
-  "Outras Tecnologias", "Docker, Git"
-)
-
-aprendendo <- tribble(
-  ~area, ~skills,
-  "Linguagens de Programação", "Python",
-  "Dados", "SQL"
-)
 
 habilidades <- tribble(
   ~title, ~unit, ~startMonth, ~startYear, ~endMonth, ~endYear, ~where, ~detail,
